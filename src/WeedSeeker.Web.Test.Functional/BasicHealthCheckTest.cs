4c4e6367696dabde2ad7847564eabca25675427d
Andrey <kllzn@ya.ru>
2016-02-03 17:18:15 +0300

    added basic order test

--- src/WeedSeeker.Web.Test.Functional/BasicHealthCheckTest.cs
+++ src/WeedSeeker.Web.Test.Functional/BasicHealthCheckTest.cs
@@ -15,45 +15,8 @@ namespace WeedSeeker.Web.Test.Functional
     /// Simple health check test. It opens the site and checks if the site is up.
     /// </summary>
     [TestFixture]
-    public class BasicHealthCheckTest
+    public class BasicHealthCheckTest : BaseTest
     {
-        /// <summary>
-        /// Gets the instance of Selenium <see cref="IWebDriver"/>.
-        /// </summary>
-        /// <value>The current instance of Selenium <see cref="IWebDriver"/>.</value>
-        public IWebDriver Driver { get; private set; }
-        
-        /// <summary>
-        /// Gets the root URL of the application being tested.
-        /// </summary>
-        /// <value>The root URL of the application being tested.</value>
-        public Uri RootUrl { get; private set; }
-
-        /// <summary>
-        /// Sets up before executing all tests in this fixture
-        /// </summary>
-        [OneTimeSetUp]
-        public void SetUpEnvironmentBeforeAllTests()
-        {
-            //
-            // Currently using ChromeDriver directly. 
-            // In the future, we should encapsulate it and use Autofac 
-            // dependency injection to instantiate any browser driver.
-            //
-            Driver = new ChromeDriver();
-            RootUrl = Settings.Default.RootUrl;
-        }
-
-        /// <summary>
-        /// Setups the environment before each test.
-        /// </summary>
-        [SetUp]
-        public void SetupEnvironmentBeforeEachTest()
-        {
-            Driver.Navigate().GoToUrl(RootUrl);
-        }
-
-
         /// <summary>
         /// This test opens the home page and checks whether the URL is correct
         /// </summary>
@@ -65,9 +28,6 @@ namespace WeedSeeker.Web.Test.Functional
             Assert.That(Driver.Url, Is.EqualTo(RootUrl));
 
         }
-
-
-
 
         /// <summary>
         /// Opens the home page and verifies whether that page is actually the home.
@@ -81,25 +41,5 @@ namespace WeedSeeker.Web.Test.Functional
 
         }
 
-        /// <summary>
-        /// Cleans up enviroment after each test.
-        /// </summary>
-        [TearDown]
-        public void CleanUpEnviromentAfterEachTest()
-        {
-            // Right now, the only cleanup action that happens is a simple logout.
-            Driver.Navigate().GoToUrl("/wp-login.php?action=logout");
-        }
-
-
-
-        /// <summary>
-        /// Cleans up the environment after executing all tests in this fixture.
-        /// </summary>
-        [OneTimeTearDown]
-        public void CleanUpEnvironmentAfterAllTests()
-        {
-            Driver?.Quit();
-        }
     }
 }
---
+++ src/WeedSeeker.Web.Test.Functional/BasicOrderTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace WeedSeeker.Web.Test.Functional
+{
+    [TestFixture]
+    public class BasicOrderTest : BaseTest
+    {
+        [Test]
+        public void OpenHomePageAndCheckOrderSubmitSuccess()
+        {
+            var prodNameElement = Driver.FindElement( By.Id( "search-term" ) );
+            prodNameElement.SendKeys( "example" );
+
+            var amountElement = Driver.FindElement( By.Id( "amount" ) );
+            amountElement.SendKeys( "0,01" );
+
+            var priceElement = Driver.FindElement( By.Id( "price" ) );
+            priceElement.SendKeys( "1" );
+
+            var submitButton = Driver.FindElement( By.ClassName( "btn-success" ) );
+            submitButton.Click();
+
+            var loginElement = Driver.FindElement( By.Id( "username" ) );
+            var passwordElement = Driver.FindElement( By.Id( "password" ) );
+            
+            Assert.IsNotNull(loginElement);
+            Assert.IsNotNull( passwordElement );
+        }
+    }
+}
